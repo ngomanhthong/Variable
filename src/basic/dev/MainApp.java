a0cc471d070a9deb52adb4267234384619b0befe
ngomanhthong <113655638+ngomanhthong@users.noreply.github.com>
2022-10-06 10:55:03 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,29 @@
+package basic.dev;
+/**
+ * 
+ * @author NgoMAnhThong
+ *
+ */
+public class MainApp {
+	public static void main(String[] args) {
+		// khai báo biến 
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho Va Ten: ");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Old: " + old);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		
+		//Khaibáo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+		
+		
+	}
+
+}
